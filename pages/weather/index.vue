cf903eb49fad2421c87e666ca827a9da0693e340
354224080@qq.com <huyongdi525>
2020-11-30 15:52:50 +0800

    测试 tor

--- pages/weather/index.vue
+++ pages/weather/index.vue
@@ -8,12 +8,33 @@
 	export default {
 		data() {
 			return {
-			
+				
 			}
 		},
-		onLoad: function() {},
-		methods: {
+		onLoad: function() {
+			initWeather()
+		},
 		
+		methods: {
+			// 根据手机位置初始化天气
+			initWeather(){
+				console.log(222)
+			},
+			// 请求百度接口获取天气
+			getWeather(){
+				uni.request({
+					url: 'http://api.map.baidu.com/telematics/v3/weather?location=%E5%8C%97%E4%BA%AC&output=json&ak=pSI4uAcN7VMFdc1jCSQDwxS3KxGghGgz',
+					data: {
+						text: 'uni.request'
+					},
+					header: {
+						'custom-header': 'hello' //自定义请求头信息
+					},
+					success: (res) => {
+						console.log(res);
+					}
+				});
+			}
 		}
 	}
 </script>
