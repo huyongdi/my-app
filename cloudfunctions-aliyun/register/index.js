3695d3419f379ae95c9ae8b07a73cf302036aadc
354224080@qq.com <huyongdi525>
2020-12-02 15:46:34 +0800

    增加注册功能

---
+++ cloudfunctions-aliyun/register/index.js
@@ -0,0 +1,18 @@
+'use strict';
+const db = uniCloud.database()
+exports.main = async (event, context) => {
+	const collection = db.collection('uni-id-users')
+	const res = await collection.where({
+		username: event.username,
+		password: event.password
+	}).count()
+	console.log(res)
+	if (res.total) {
+		return {
+			status: 500,
+			msg: '用户名已存在！'
+		}
+	}
+	let res2 = await collection.add(event)
+	return res2
+};
