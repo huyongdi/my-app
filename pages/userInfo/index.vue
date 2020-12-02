c25d38dd4a56d769f92bd13793f6af7bba1267dc
354224080@qq.com <huyongdi525>
2020-12-02 17:17:29 +0800

    我的页面开发

--- pages/userInfo/index.vue
+++ pages/userInfo/index.vue
@@ -1,6 +1,19 @@
 <template>
 	<view class="wearther-content">
-		<view @click="logout">退出登录</view>
+		<view class="top">
+			<image class="top-img" src="@/static/img/my-user.png"></image>
+			<view class="right-name">
+				<view class="nick-name">我是毛衣</view>
+				<view class="sig">这里放个性签名</view>
+			</view>
+		</view>
+		
+		<view class="op-lists">
+			<view class="list" @click="logout">
+				<image class="list-img" src="@/static/img/logout.png"></image>
+				退出登录
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -14,6 +27,7 @@
 		onLoad: function() {},
 		methods: {
 			logout() {
+				uni.clearStorage()
 				uni.redirectTo({
 					url: '/pages/login/index'
 				});
@@ -23,5 +37,50 @@
 </script>
 
 <style scoped lang="less">
+	.wearther-content {
+		background-color: rgba(237, 237, 237);
 
+		.top {
+			padding: 60rpx 40rpx;
+			display: flex;
+			background-color: #fff;
+			margin-bottom: 20rpx;
+			.top-img {
+				width: 120rpx;
+				height: 120rpx;
+				margin-right: 30rpx;
+			}
+
+			.right-name {
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+
+				.nick-name {
+					font-size: 40rpx;
+					font-weight: bold;
+				}
+
+				.sig {
+					color: #999;
+				}
+			}
+		}
+		.op-lists{
+			background-color: #fff;
+			.list{
+				border-bottom: 1px solid #d3d3d3;
+				height: 100rpx;
+				line-height: 100rpx;
+				padding: 0 40rpx;
+				display: flex;
+				align-items: center;
+				.list-img{
+					width: 60rpx;
+					height: 60rpx;
+					margin-right: 30rpx;
+				}
+			}
+		}
+	}
 </style>
